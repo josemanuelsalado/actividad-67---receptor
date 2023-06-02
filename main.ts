b045897c1aadf09bed8239b38fa60dd94d083d5f
josemanuelsalado <132547206+josemanuelsalado@users.noreply.github.com>
2023-06-02 12:41:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+radio.onReceivedNumber(function (receivedNumber) {
+    led.setBrightness(led.brightness() + receivedstring)
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedstring == "POWER") {
+        encendido = !(encendido)
+        led.setBrightness(125)
+        led.enable(encendido)
+    }
+})
+let receivedstring = ""
+let encendido = false
+radio.setGroup(1)
+encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
